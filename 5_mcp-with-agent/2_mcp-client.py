ec80945a8044e75d5270e5d31cfbed7983764df2
wedding-crasher <hangu4043@gmail.com>
2025-07-14 10:57:52 +0000

    [feat] add 3_multi-agent-collaboration

--- 5_mcp-with-agent/2_mcp-client.py
+++ 5_mcp-with-agent/2_mcp-client.py
@@ -1,7 +1,7 @@
 import asyncio
 from fastmcp import Client
 
-SERVER_URL = "http://127.0.0.1:8000/mcp"
+SERVER_URL ="http://127.0.0.1:8000/mcp"
 client = Client(SERVER_URL)
 
 async def main():
@@ -10,17 +10,18 @@ async def main():
             print(" Pinging MCP server...")
             await client.ping()
             print(" MCP server is alive.\n")
-
+            tools = await client.list_tools()
+            print(f"Available tools: {tools}")
             # 1. Search Tool
             print("############## Calling 'search' tool...######################")
             result = await client.call_tool("search", {"query": "What is quantum computing?"})
             print("\n Search Result:\n", result)
 
-            # 2. Fetch Content Tool
-            print("\n############ Calling 'fetch_content' tool...################")
-            test_url = "https://en.wikipedia.org/wiki/Quantum_computing"
-            content_result = await client.call_tool("fetch_content", {"url": test_url})
-            print("\n Fetched Web Content:\n", content_result[:1000], "\n... (truncated)")
+            # # 2. Fetch Content Tool
+            # print("\n############ Calling 'fetch_content' tool...################")
+            # test_url = "https://en.wikipedia.org/wiki/Quantum_computing"
+            # content_result = await client.call_tool("fetch_content", {"url": test_url})
+            # print("\n Fetched Web Content:\n", content_result[:1000], "\n... (truncated)")
 
     except Exception as e:
         print(" Error during client test:", e)
